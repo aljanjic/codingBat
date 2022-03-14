970c9cd695cddad2a0a80b3d06be690186317abc
Aljoša Janjić <96263471+aljanjic@users.noreply.github.com>
2022-03-14 23:43:58 +0100

    Delete Java directory

--- Java/src/functional_1/Square.java
+++
@@ -1,8 +0,0 @@
-public List<Integer> square(List<Integer> nums) {
-        //   nums.replaceAll(n -> n * n);
-        // return nums;
-        
-        return nums.stream()
-          .map(n -> n*n)
-          .collect(Collectors.toList());
-}
--- Java/src/warmup_1/MixStart.java
+++
@@ -1,9 +0,0 @@
-//Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
-//
-//        mixStart("mix snacks") → true
-//        mixStart("pix snacks") → true
-//        mixStart("piz snacks") → false
-
-public boolean mixStart(String str) {
-        return ((str.indexOf("ix") == 1));
-        }
